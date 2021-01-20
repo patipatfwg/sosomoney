cf422a3de0a44ac480ebd9091d8879b5b643f42d
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2021-01-20 09:37:10 +0700

    937

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
         "host": config.DB_HOST,
         "dialect": config.DB_DIALECT
     },
-    "heroku": {
+    "production": {
         "username": config.DB_USERNAME_HEROKU,
         "password": config.DB_PASSSWORD_HEROKU,
         "database": config.DB_DATABASE_HEROKU,
