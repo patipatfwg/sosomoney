b9811df8642bfa54be62924297a2bcb34bced657
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2021-01-20 09:33:48 +0700

    933

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const basicAuth = require('express-basic-auth');
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 app.use(
     '/users',
